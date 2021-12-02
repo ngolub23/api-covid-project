f513c56281788cbee6095dafd9478c6d8405e4de
patrickjadams1941 <patrickjadams1941@users.noreply.github.com>
2021-12-01 20:07:17 -0500

    New functionality

--- UpdateCase/index.js
+++ UpdateCase/index.js
@@ -1,17 +1,18 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var  config = require('./dbconfig');
+const  sql = require('mssql');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
-};
+
+module.exports = async function (context, req) {
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log(result.affectedRows + " record(s) updated");
+        });
+      });
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
